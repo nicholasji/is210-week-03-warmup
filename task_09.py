db4056da1351fc9f1cbd66e00b2d91a8f6d98f09
Nick Ileczko <nickileczko@gmail.com>
2015-09-11 08:16:49 +0000

    task9

--- task_09.py
+++ task_09.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Perhaps you have wondered how predictable machines like computers can generate randomness.
-In reality, most random numbers used in computer programs are pseudo-random,
-which means they are generated in a predictable fashion using a mathematical formula.
-This is fine for many purposes, but it may not be random in the way you expect i
-f you're used to dice rolls and lottery drawings.A computer that can build the question."""
+"""Perhaps you have wondered how predictable machines like computers can
+generate randomness.In reality, most random numbers used in computer
+programs are pseudo-random,which means they are generated in a predictable
+fashion using a mathematical formula.This is fine for many purposes,
+but it may not be random in the way you expect if you're used to dice rolls
+and lottery drawings.A computer that can build the question."""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
