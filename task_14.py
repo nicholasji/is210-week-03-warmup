adb4cb1f89a4dcc786899d0bb30f9ba4ea8b24f9
Nick Ileczko <nickileczko@gmail.com>
2015-09-11 05:58:18 +0000

    true AND false

---
+++ task_14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""TRUE, FALSE, AND"""
+
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+
+INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
+
+print INTEGER_EQUIV
