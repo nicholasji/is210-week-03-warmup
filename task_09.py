cee54198e6cb67b548f198ec183b645f8c4d2742
Nick Ileczko <nickileczko@gmail.com>
2015-09-11 08:00:16 +0000

    task09

--- task_09.py
+++ task_09.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""Perhaps you have wondered how predictable machines like computers can generate randomness.
+In reality, most random numbers used in computer programs are pseudo-random,
+which means they are generated in a predictable fashion using a mathematical formula.
+This is fine for many purposes, but it may not be random in the way you expect i
+f you're used to dice rolls and lottery drawings.A computer that can build the question."""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
-
-FDR = ('As Commander in Chief of the Army and Navy,'
-       'I have directed that al'
-       'measures be taken for our defense.'
-       'But always will our whole nation '
-       'remember the character of the onslaught'
-       'against us.'
-       'No matter how long it may take us to'
-       'overcome this premeditated'
-       'invasion, the American people in their'
-       'righteous might will win through'
-       'to absolute victory.')
