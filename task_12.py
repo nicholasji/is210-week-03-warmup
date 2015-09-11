ac719724f4a8f4b1c3d7a611d78624bf4b4cfd1b
Nick Ileczko <nickileczko@gmail.com>
2015-09-11 05:56:14 +0000

    decimal and fractions

---
+++ task_12.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""INT, FLOAT, DEC, AND FRAC"""
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = '0.1'
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
+
+print INTVAL
+
+print FLOATVAL
+
+print DECVAL
+
+print FRACVAL
