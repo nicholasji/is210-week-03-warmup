e826a8af0fbf4101aa02a43ed345904466cfbe66
Nick Ileczko <nickileczko@gmail.com>
2015-09-11 05:52:12 +0000

    docstring

--- task_09.py
+++ task_09.py
@@ -5,3 +5,15 @@
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
+
+FDR = ('As Commander in Chief of the Army and Navy,'
+       'I have directed that al'
+       'measures be taken for our defense.'
+       'But always will our whole nation '
+       'remember the character of the onslaught'
+       'against us.'
+       'No matter how long it may take us to'
+       'overcome this premeditated'
+       'invasion, the American people in their'
+       'righteous might will win through'
+       'to absolute victory.')
